cb9da13ebaea397a0a0cd70af8dd92a563844f46
Wellington Pereira <wellington.pereira@a3data.com.br>
2025-02-14 19:30:44 -0300

    chore(prettier): adjust eslint file to use prettier plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 import { FlatCompat } from '@eslint/eslintrc'
+import prettierPlugin from 'eslint-plugin-prettier'
+import prettier from 'prettier'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
@@ -18,10 +20,13 @@ const eslintConfig = [
   ),
   {
     plugins: {
-      prettier: import('eslint-plugin-prettier')
+      prettier: prettierPlugin,
     },
     rules: {
-      'prettier/prettier': 'error',
+      'prettier/prettier': [
+        'error',
+        (await prettier.resolveConfig('./.prettierrc')) ?? {},
+      ],
       '@typescript-eslint/no-unused-vars': 'error',
       '@typescript-eslint/no-explicit-any': 'error',
     },
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,28 +10,28 @@ export default {
     extend: {
       colors: {
         primary: {
-          light: "#9A7FDD",
-          DEFAULT: "#6C529E",
-          dark: "#B49AFF",
+          light: '#9A7FDD',
+          DEFAULT: '#6C529E',
+          dark: '#B49AFF',
         },
         secondary: {
-          light: "#2081E2",
-          DEFAULT: "#186CC0",
-          dark: "#0C4883",
+          light: '#2081E2',
+          DEFAULT: '#186CC0',
+          dark: '#0C4883',
         },
         background: {
-          light: "#E0E3FF",
-          dark: "#090A10",
+          light: '#E0E3FF',
+          dark: '#090A10',
         },
         gradient: {
-          light: "#B49AFF",
-          dark: "#6574FF",
+          light: '#B49AFF',
+          dark: '#6574FF',
         },
       },
       fontSize: {
-        'xxs': ['0.625rem', '0.875rem'],
-        'xxxs': ['0.5rem', '0.75rem'],
-        'xxxxs': ['0.375rem', '0.5rem'],
+        xxs: ['0.625rem', '0.875rem'],
+        xxxs: ['0.5rem', '0.75rem'],
+        xxxxs: ['0.375rem', '0.5rem'],
       },
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
