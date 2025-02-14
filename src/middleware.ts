7a4f2b173d83c3512b2942a4d6d23ed0c57bf1e7
Wellington Pereira <wellington.pereira@a3data.com.br>
2025-02-14 19:36:45 -0300

    feat(project): create a middleware to improve usability and performance

---
+++ src/middleware.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export function middleware(request: NextRequest) {
+  const response = NextResponse.next()
+
+  response.headers.set('X-Frame-Options', 'DENY')
+  response.headers.set('X-Content-Type-Options', 'nosniff')
+  response.headers.set('Referrer-Policy', 'origin-when-cross-origin')
+
+  if (request.nextUrl.pathname.startsWith('/_next/static')) {
+    response.headers.set('Cache-Control', 'public, max-age=31536000, immutable')
+  }
+
+  return response
+}
