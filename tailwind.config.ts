bbcfbf4cd064d1873b4ff541320e95808f0afa04
Wellington Pereira <wellington.pereira@a3data.com.br>
2025-02-14 19:24:57 -0300

    chore(setup): adjust initial setup by project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,43 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: {
+          light: "#9A7FDD",
+          DEFAULT: "#6C529E",
+          dark: "#B49AFF",
+        },
+        secondary: {
+          light: "#2081E2",
+          DEFAULT: "#186CC0",
+          dark: "#0C4883",
+        },
+        background: {
+          light: "#E0E3FF",
+          dark: "#090A10",
+        },
+        gradient: {
+          light: "#B49AFF",
+          dark: "#6574FF",
+        },
+      },
+      fontSize: {
+        'xxs': ['0.625rem', '0.875rem'],
+        'xxxs': ['0.5rem', '0.75rem'],
+        'xxxxs': ['0.375rem', '0.5rem'],
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        dmsans: ['DM Sans', 'sans-serif'],
       },
     },
   },
   plugins: [],
-} satisfies Config;
+} satisfies Config
